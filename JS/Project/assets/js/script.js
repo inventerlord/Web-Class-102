33cfdba938cf20293d3b5976934f00df85091abe
inventerlord <inventerlord@gmail.com>
2021-05-30 20:00:26 +0430

    final-project

--- JS/Project/assets/js/script.js
+++ JS/Project/assets/js/script.js
@@ -1,8 +1,17 @@
-bannerSlider();
+let bannerIndex = 0;
+bannerSlides();
 
-function bannerSlider(){
-    let banners = document.getElementsByClassName('banner-Items');
-    for(i = 0; i <= banners.length;i++){
-        banners[i].style.display = "none";
-    }
+function bannerSlides(){
+    let i;
+  let banners = document.getElementsByClassName("banner-Items");
+  
+  for (i = 0; i < banners.length; i++) {
+    banners[i].style.display = "none";  
+  }
+  bannerIndex++;
+  if (bannerIndex > banners.length) {bannerIndex = 1}    
+  
+  banners[bannerIndex-1].style.display = "block"; 
+  setTimeout(bannerSlides, 2000); 
+    
 }
