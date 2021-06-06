f95a0d283fb7535b54411428c2414e8a4d0f93b5
inventerlord <inventerlord@gmail.com>
2021-06-06 19:33:29 +0430

    Lec017

--- JS/Project/assets/js/script.js
+++ JS/Project/assets/js/script.js
@@ -1,17 +1,34 @@
-let bannerIndex = 0;
-bannerSlides();
+// let bannerIndex = 0;
+// bannerSlides();
 
-function bannerSlides(){
-    let i;
-  let banners = document.getElementsByClassName("banner-Items");
+// function bannerSlides(){
+//     let i;
+//   let banners = document.getElementsByClassName("banner-Items");
   
-  for (i = 0; i < banners.length; i++) {
-    banners[i].style.display = "none";  
-  }
-  bannerIndex++;
-  if (bannerIndex > banners.length) {bannerIndex = 1}    
+//   for (i = 0; i < banners.length; i++) {
+//     banners[i].style.display = "none";  
+//   }
+//   bannerIndex++;
+//   if (bannerIndex > banners.length) {bannerIndex = 1}    
   
-  banners[bannerIndex-1].style.display = "block"; 
-  setTimeout(bannerSlides, 2000); 
+//   banners[bannerIndex-1].style.display = "block"; 
+//   setTimeout(bannerSlides, 2000); 
     
+// }
+let startValue = 0;
+
+switchBanner();
+function switchBanner(){
+  let i;
+  let allBanners = document.getElementsByClassName("banner-Items");
+
+  for(i=0 ; i< allBanners.length ;i++){
+    allBanners[i].style.display = "none";
+  }
+  startValue++;
+  if (startValue > allBanners.length) {
+    startValue = 1;
+  }
+  allBanners[startValue-1].style.display = "Block";
+  setTimeout(switchBanner,2000);
 }
